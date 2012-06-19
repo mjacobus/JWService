f1fe8fd4376f6866d3b7ca8895adc5f5b89187ec
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-06-19 19:10:39 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'ruby-prof'
+  gem 'guard-rspec'
+  gem 'rspec-rails', '~> 2.6'
+  gem 'capybara'
+  gem 'factory_girl'
+end
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', '0.8.2', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JWService::Application.config.secret_token = 'd21a365e06e630988579d877fefd78705ce62615f2969670be9b1b926530d48adff66589974ab9f6ba5ead21f6fa5255297bf36ad12d4c4a111c55bcf484b39f'
